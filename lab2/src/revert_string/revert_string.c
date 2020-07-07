a3e08bfae1264921db7cef52e7c1aa270b2d0da2
sonyarosha <sonyarosha0@gmail.com>
2020-07-07 11:06:57 +0000

    Added rev_str_program, modified: revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stdlib.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int medium = strlen(str)/2;
+    for (int i = 0; i < medium; ++i) {
+		char temp = str[i];
+		str[i] = str[strlen(str) - i - 1];
+		str[strlen(str) - i - 1] = temp;
+    }
 }
-
